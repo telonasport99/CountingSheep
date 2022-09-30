5e595b6a14fe72e3b9d327e9678e3012f8e1c4f3
telonasport99 <telonasport99@gmail.com>
2022-09-30 20:39:50 +0300

    met2

---
+++ met2.js
@@ -0,0 +1,9 @@
+function countSheeps(arrayOfSheep) {
+    var num = 0;
+
+    for(var i = 0; i < arrayOfSheep.length; i++)
+        if(arrayOfSheep[i] == true)
+            num++;
+
+    return num;
+}
